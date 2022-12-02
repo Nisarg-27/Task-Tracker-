2fd99c5a53e1e727b1d0a8861e74c16c11b4dab9
Nisarga Ovhal <nisargaovhal@gmail.com>
2022-12-03 03:36:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import { Header } from "./components/Header";
-
+import { useState } from "react";
+import Tasks from "./components/Tasks"
 function App() {
-  const name = "Nisarga";
-  const x = false;
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Online Shopping',
+        day: 'Feb 5th at 02:30pm',
+        reminder: true, 
+    },
+    {
+        id: 2,
+        text: 'Online Shopping',
+        day: 'Feb 5th at 02:30pm',
+        reminder: false, 
+    },
+    {
+        id: 3,
+        text: 'Online Shopping',
+        day: 'Feb 5th at 02:30pm',
+        reminder: false, 
+    }
+]);
   return (
    <div className="container">
-    <Header title={name} bol={x}></Header>
-
+    <Header></Header>
+    <Tasks tasks={tasks}></Tasks>
    </div>
   );
 }
