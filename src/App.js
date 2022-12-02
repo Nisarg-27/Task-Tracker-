3f3fab4cee06e82dfb38513e103f33e976491156
Nisarga Ovhal <nisargaovhal@gmail.com>
2022-12-02 20:07:47 +0530

    initiaal commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
-import { Tasks } from "./components/Tasks";
+
 function App() {
   const name = "Nisarga";
   const x = false;
   return (
    <div className="container">
     <Header title={name} bol={x}></Header>
-    <Tasks />
+
    </div>
   );
 }
